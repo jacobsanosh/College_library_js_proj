7d1a9094fbc789101fe0bbe194e94146306402bc
jacobsanosh <jacobsanosh@gmail.com>
2022-07-06 19:02:57 +0530

    CRUD using local storage

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 console.log("connected")
+    //for editing form
+let e_form = document.getElementById('edititng_form');
+e_form.style.display = "none"
+
+
+
 
 class Book {
-    constructor(b_name, b_author, b_type) {
+    constructor(b_name, b_author, b_type, owned) {
         this.b_name = b_name;
         this.b_author = b_author;
         this.b_type = b_type
+        this.b_own = owned
     }
 }
 class Display {
@@ -22,6 +29,8 @@ class Display {
         form.reset();
         // console.log("entered in clear")
     }
+
+
     show_books() {
         // console.log("connected to books")
         let h1 = document.getElementById('details')
@@ -37,18 +46,23 @@ class Display {
             h1.innerHTML = "your books are now"
                 // table.style.display = "block";
             let string = "";
-            libObj.forEach(element => {
+            libObj.forEach((element, index) => {
                 let tbody = document.getElementById("tbody");
                 string += `<tr>
                 <td>${element.b_name}</td>
                 <td>${element.b_author}</td>
                 <td>${element.b_type}</td>
+                <td><button type="button" id=${index} class="btn btn-success" onclick="edit_lib(${index})">Edit</button>
+                <button type="button" id=${index} class="btn btn-danger" onclick="delete_lib(${index})">Delete</button>
+                </td>
+                <td id="own${index}">${element.b_own}</td>
               </tr>`
             });
             tbody.innerHTML = string;
         } else {
             // table.style.display = "none";
             h1.innerHTML = "Nothing is here"
+            tbody.innerHTML = ""
         }
     }
     add(book) {
@@ -62,12 +76,49 @@ class Display {
         }
         libObj.push(book)
         localStorage.setItem('lib', JSON.stringify(libObj))
+
         this.show_books()
     }
 }
 let d = new Display()
 d.show_books()
-    //creatin an submit evenlistener
+
+function edit_lib(a) {
+    console.log("oon Edit haha")
+    let submit = document.getElementById('edititng_form')
+    let lediting = document.getElementById('lediting')
+    e_form.style.display = "block"
+    let lib = localStorage.getItem('lib');
+    let libObj = JSON.parse(lib);
+    console.log("libobj", libObj[a])
+    lediting.innerHTML = "name of the book is " + libObj[a].b_name
+    submit.addEventListener('submit', (event) => {
+        event.preventDefault()
+            // console.log("submitted")
+        let owned = document.getElementById('ownedby').value;
+        libObj[a].b_own = owned;
+        // console.log(libObj[a].b_own)
+        localStorage.setItem('lib', JSON.stringify(libObj))
+        e_form.style.display = "none"
+        d.show_books();
+
+    })
+}
+//for deleteing lib books
+function delete_lib(a) {
+    let lib = localStorage.getItem('lib')
+    let libObj
+    if (lib == null) {
+        libObj = []
+    } else {
+        libObj = JSON.parse(lib);
+    }
+    libObj.splice(a, 1)
+    localStorage.setItem('lib', JSON.stringify(libObj))
+    d.show_books()
+}
+
+//creatin an submit evenlistener
 
 let f_submit = document.getElementById("BK_form");
 f_submit.addEventListener('submit', (event) => {
@@ -88,7 +139,7 @@ f_submit.addEventListener('submit', (event) => {
     }
     console.log(type, b_name, b_author)
 
-    let book = new Book(b_name, b_author, type)
+    let book = new Book(b_name, b_author, type, "avaliable")
     console.log(book)
         //used for displaying the book
 
